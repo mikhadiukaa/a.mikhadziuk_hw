6aea635e741385ba498784ed5ee5e48395ed216b
mikhadiukaa <91308025+mikhadiukaa@users.noreply.github.com>
2024-07-06 18:10:07 +0300

    Add files via upload

--- HW3/game_single_dice.cpp
+++ HW3/game_single_dice.cpp
@@ -8,10 +8,10 @@ if (i%2 == 0){
 score_1 += dice_1();
 }
 else{
-score_2 +=dice_2();
+score_2 +=dice_1();
 }
 std::cout << "Step:" << i + 1 << "; Player 1: " << score_1 << "; Player 2: " << score_2 << std::endl;
-if ((score_1> 50) || (score_2 > 50)){
+if ((score_1>= 50) || (score_2 >= 50)){
 if (score_1 > score_2){
 std::cout << "Player 1 won" << std::endl;
 }
@@ -24,7 +24,7 @@ else{
 break;
 }
 if (i==99){
-if ((score_1<=50)&&(score_2<=50)){
+if ((score_1<50)&&(score_2<50)){
     std::cout << "Drawn game" << std::endl;
 }
 }
