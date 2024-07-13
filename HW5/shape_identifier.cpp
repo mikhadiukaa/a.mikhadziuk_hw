27f6f0e0d31329442135f5e7be261038a1be5924
mikhadiukaa <mikhadiuk.aa@phystech.edu>
2024-07-13 13:00:50 +0300

    HW3

---
+++ HW5/shape_identifier.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+namespace Shapes {
+    enum class Shape {
+        UNKNOWN = -1,
+        CIRCLE,
+        SQUARE,
+        TRIANGLE
+    };
+
+    void printShapes(Shapes::Shape SHAPE_NAME) {
+        if (SHAPE_NAME == Shapes::Shape::SQUARE) {
+            std::cout << "SQUARE";
+        }
+        else if (SHAPE_NAME == Shapes::Shape::CIRCLE) {
+            std::cout << "CIRCLE";
+        }
+        else if (SHAPE_NAME == Shapes::Shape::TRIANGLE) {
+            std::cout << "TRIANGLE";
+        }
+        else {
+            std::cout << "UNKNOWN";
+        }
+    }
+
+    Shapes::Shape recognizeShapes(const std::string &SHAPE_INPUT) {
+        Shapes::Shape SHAPE_NAME;
+        if (SHAPE_INPUT == "CIRCLE") { SHAPE_NAME = Shapes::Shape::CIRCLE; } else if (SHAPE_INPUT == "SQUARE") {
+            SHAPE_NAME = Shapes::Shape::SQUARE;
+        } else if (SHAPE_INPUT == "TRIANGLE") { SHAPE_NAME = Shapes::Shape::TRIANGLE; } else {
+            SHAPE_NAME = Shapes::Shape::UNKNOWN;
+        }
+        return SHAPE_NAME;
+    }
+}
+
+
+int main() {
+    std::string SHAPE_NAME;
+    std::cout << "Please enter the name of the geomtric shape using capital letters" << std::endl;
+    std::cin >> SHAPE_NAME;
+    Shapes::printShapes(Shapes::recognizeShapes(SHAPE_NAME));
+    return 0;
+}
